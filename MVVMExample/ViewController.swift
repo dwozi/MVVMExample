c769a801a33b967323c97941e5b117ed5514208c
Hakan Hardal <hakanhardal@icloud.com>
2024-03-12 15:58:55 +0300

    Initial Commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Hakan Hardal on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
